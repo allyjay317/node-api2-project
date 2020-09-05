77c37265069e7067b8952018b6ffbf12c5aa83a0
Allison Usher <allyjay317@gmail.com>
2020-09-04 19:41:07 -0700

    completed get => /api/posts/:id

--- index.js
+++ index.js
@@ -79,20 +79,13 @@ server.get('/api/posts/:id', (req, res) => {
       if (result.length === 0) {
         res.status(404).json({ message: "The post with the specified ID does not exist." })
       }
-      res.status(200).json(result)
+      else {
+        res.status(200).json(result)
+      }
     })
     .catch(err => {
       res.status(500).json({ error: "The post information could not be retrieved." })
     })
-  //   - If the _post_ with the specified `id` is not found:
-
-  //   - return HTTP status code `404` (Not Found).
-  //   - return the following JSON object: `{ message: "The post with the specified ID does not exist." }`.
-
-  // - If there's an error in retrieving the _post_ from the database:
-  //   - cancel the request.
-  //   - respond with HTTP status code `500`.
-  //   - return the following JSON object: `{ error: "The post information could not be retrieved." }`
 })
 server.get('/api/posts/:id/comments', (req, res) => {
   //   - If the _post_ with the specified `id` is not found:
